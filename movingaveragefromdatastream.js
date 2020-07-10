5e89cb2049e3288649d26ebbcc2b741c1e571a2b
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-10 06:17:30 -0400

    solutions

---
+++ movingaveragefromdatastream.js
@@ -0,0 +1,31 @@
+//Objective is to design a class that will add values to it's base
+//and return it's average as new values are being added in. Once 
+//the class reaches full capacity, it removes the first element added in
+
+class Average {
+    constructor(size) {
+        this.size = size 
+        this.queue = []
+        this.result = 0
+    }
+
+    next(val) {
+        //Add the value to the queue unconditionally
+        this.queue.push(val)
+
+        //If the size is 3 but we have 4 elements,
+        //we need to remove the first element and thus
+        //subtract that element from our result as well
+        while (this.queue.length > this.size) {
+            this.result -= this.queue.shift()
+        }
+
+        //Add the value to the result unconditionally
+        this.result += val 
+        return this.result / this.queue.length 
+    }
+}
+
+let test = new Average(4)
+test.next(5)
+test.next(7)
